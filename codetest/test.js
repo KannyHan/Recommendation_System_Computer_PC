10b851560e91fc7c829de81229bcae1414de6e94
KannyHan <sarawutpongsumalee@gmail.com>
2024-11-27 13:54:17 +0700

    save

--- codetest/test.js
+++ codetest/test.js
@@ -1,234 +1,14 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const bcrypt = require('bcrypt');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 const { Parser } = require('json2csv');
 const fs = require('fs');
-const puppeteer = require('puppeteer');
-const _ = require('lodash');
-const dns = require('dns');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-dns.setServers(['8.8.8.8', '1.1.1.1']);
-// เชื่อมต่อกับ MongoDBmongodb+srv://kenny:Bihbk4EGAj6JwqxZ@cluster0.olj3q.mongodb.net/
-mongoose.connect('mongodb+srv://kenny:Bihbk4EGAj6JwqxZ@cluster0.olj3q.mongodb.net/spec?retryWrites=true&w=majority&appName=Cluster0', { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((error) => {
-    console.log("Error connecting to MongoDB:", error);
-});
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.urlencoded({ extended: true }));
-// กำหนด Schema
-const computerSchema = new mongoose.Schema({
-    BrandCPU: String,
-    SeriesCPU: String,
-    ModelCPU: String,
-    CPU_Base_Clock: Number,
-    PriceCPU: Number,
-    BrandMainboard: String,
-    ModelMainboard: String,
-    Mainboard_CPU_Support: String,
-    MemoryMainboard: Number,
-    Mainboard_Memory_Support: String,
-    PriceMainboard: Number,
-    BrandVGA: String,
-    ChipsetVGA: String,
-    SeriesVGA: String,
-    ModelVGA: String,
-    VGA_Base_Clock: Number,
-    VGA_Boost_Clock: Number,
-    VGA_Memory_Clock: Number,
-    VGA_Memory_Size: Number,
-    PriceVGA: Number,
-    RAM_Size: Number,
-    RAM_Speed: Number,
-    PriceRAM: Number,
-    CapacitySSD: Number,
-    Read_SSD: Number,
-    Write_SSD: Number,
-    PriceSSD: Number,
-    CapacitySSD2: Number,
-    Read_SSD2: Number,
-    Write_SSD2: Number,
-    PriceSSD2: Number,
-    CapacityHDD: Number,
-    Speed_HDD: Number,
-    PriceHDD: Number,
-    PS: Number,
-    PricePS: Number,
-    BrandCASE: String,
-    ModelCASE: String,
-    WeightCASE: Number,
-    I_O_Ports_CASE: String,
-    PriceCASE: Number,
-    BrandCOOLING: String,
-    ModelCOOLING: String,
-    Fan_Built_In_COOLING: String,
-    PriceCOOLING: Number,
-    BrandMONITOR: String,
-    ModelMONITOR: String,
-    Display_Size_MONITOR: Number,
-    Max_Resolution_MONITOR: String,
-    Refresh_Rate_MONITOR: Number,
-    PriceMONITOR: Number,
-    Rank1: String,
-    Rank2: String,
-    Rank3: String
-  });
-
-  const computerSchemaJIB = new mongoose.Schema({
-    ModelCom: String,
-    BrandCPU: String,
-    SeriesCPU: String,
-    ModelCPU: String,
-    CPU_Base_Clock: Number,
-    PriceCPU: Number,
-    BrandMainboard: String,
-    ModelMainboard: String,
-    Mainboard_CPU_Support: String,
-    MemoryMainboard: Number,
-    Mainboard_Memory_Support: String,
-    PriceMainboard: Number,
-    BrandVGA: String,
-    ChipsetVGA: String,
-    SeriesVGA: String,
-    ModelVGA: String,
-    VGA_Base_Clock: Number,
-    VGA_Boost_Clock: Number,
-    VGA_Memory_Clock: Number,
-    VGA_Memory_Size: Number,
-    PriceVGA: Number,
-    RAM_Size: Number,
-    RAM_Speed: Number,
-    PriceRAM: Number,
-    CapacitySSD: Number,
-    Read_SSD: Number,
-    Write_SSD: Number,
-    PriceSSD: Number,
-    CapacitySSD2: Number,
-    Read_SSD2: Number,
-    Write_SSD2: Number,
-    PriceSSD2: Number,
-    CapacityHDD: Number,
-    Speed_HDD: Number,
-    PriceHDD: Number,
-    PS: Number,
-    PricePS: Number,
-    BrandCASE: String,
-    ModelCASE: String,
-    WeightCASE: Number,
-    I_O_Ports_CASE: String,
-    PriceCASE: Number,
-    BrandCOOLING: String,
-    ModelCOOLING: String,
-    Fan_Built_In_COOLING: String,
-    PriceCOOLING: Number,
-    BrandMONITOR: String,
-    ModelMONITOR: String,
-    Display_Size_MONITOR: Number,
-    Max_Resolution_MONITOR: String,
-    Refresh_Rate_MONITOR: Number,
-    PriceMONITOR: Number,
-    Rank1: String,
-    Rank2: String,
-    Rank3: String
-  });
-// สร้างโมเดลจาก Schema
-const Spec = mongoose.model('spec_com', computerSchema, 'spec_com');
-const SpecJIB = mongoose.model('spec_com_jib', computerSchemaJIB, 'spec_com_jib');
-
-app.set('view engine', 'ejs');
-
-app.use(express.static('public'));
-
-
-// หน้าแอคมิน
-app.use(session({
-    secret: 'KennyKey', // ใช้เป็นความลับในการเข้ารหัสเซสชัน
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: 'mongodb+srv://kenny:Bihbk4EGAj6JwqxZ@cluster0.olj3q.mongodb.net/spec?retryWrites=true&w=majority&appName=Cluster0' }),
-    cookie: { maxAge: 1000 * 60 * 60 * 24 } // อายุของคุกกี้ (1 วัน)
-}));
-
-const adminSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-});
-
-const Admin = mongoose.model('com_admin', adminSchema, 'com_admin');
-
-app.get('/register', (req, res) => {
-    res.render('registerAdmin'); // หน้าลงทะเบียนสำหรับแอดมิน
-});
-
-app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10); // เข้ารหัสรหัสผ่าน
-    const newAdmin = new Admin({
-        username: username,
-        password: hashedPassword
-    });
-
-    try {
-        await newAdmin.save();
-        res.redirect('/Adminlogin'); // หลังจากสมัครสำเร็จ ให้ไปหน้าล็อกอิน
-    } catch (error) {
-        res.status(500).send('Error creating admin');
-    }
-});
-
-app.get('/Adminlogin', (req, res) => {
-    res.render('loginAsmin'); // แสดงหน้าล็อกอิน
-});
-
-app.post('/Adminlogin', async (req, res) => {
-    const { username, password } = req.body;
-    const admin = await Admin.findOne({ username: username });
-    
-    if (admin) {
-        const validPassword = await bcrypt.compare(password, admin.password);
-        if (validPassword) {
-            req.session.adminId = admin._id; // เก็บ session เมื่อเข้าสู่ระบบสำเร็จ
-            res.redirect('/admin/dashboard'); // ไปยังหน้าแอดมิน
-        } else {
-            res.status(400).send('Invalid username or password');
-        }
-    } else {
-        res.status(400).send('Invalid username or password');
-    }
-});
-
-function requireLogin(req, res, next) {
-    if (!req.session.adminId) {
-        return res.redirect('/Adminlogin'); // ถ้าไม่ได้ล็อกอินให้กลับไปหน้า login
-    }
-    next();
-}
-
-app.get('/admin/dashboard', requireLogin, (req, res) => {
-    res.render('scraping'); // หน้าหลักของแอดมิน
-});
-
-
-// ใน route handler ส่ง `res` ไปยังฟังก์ชัน scrapeData
-app.post('/scrape', async (req, res) => {
-    const url = req.body.url;
-    const list = req.body.list; // รับ URL ที่ผู้ใช้กรอก
-    const browser = await puppeteer.launch({ headless: false });
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   
-  await page.goto(url);
+  await page.goto('https://notebookspec.com/pc/ranking');
 
   // กดปุ่ม "โหลดเพิ่มเติม" จนกว่าจะไม่มีให้กดอีก หรือข้อมูลครบ 2000 รายการ
   const hrefList = [];
@@ -264,7 +44,7 @@ app.post('/scrape', async (req, res) => {
         }
       });
     }
-  } while (loadMoreButton && hrefList.length < list);
+  } while (loadMoreButton && hrefList.length < 100);
 
   console.log(`จำนวนข้อมูลทั้งหมดที่ดึงมา: ${hrefList.length}`);
 
@@ -279,6 +59,7 @@ app.post('/scrape', async (req, res) => {
       const products = {};
       const items = document.querySelectorAll('div.my-list-wrapper ul.my-list-item li.spec-item');
       items.forEach(item => {
+        console.log(item)
         const link = item.querySelector('.name a');
         const key = item.querySelector('.type')?.innerText.trim().replace(":", "").replace(" ", "");
         if (link && key) {
@@ -296,7 +77,6 @@ app.post('/scrape', async (req, res) => {
   
   // วนลูปดึงข้อมูลจากแต่ละลิงก์ใน hrefList
   for (let index = 0; index < hrefList.length; index++) {
-    console.log(`เครื่องที่ ${index}`, hrefList[index])
     const productLinks = await scrapeLinks(hrefList[index], browser);
     productData.push(productLinks);
   }
@@ -349,9 +129,9 @@ app.post('/scrape', async (req, res) => {
         console.error(`Error scraping ${productUrl}:`, error.message);
         return null;
     }
-  }
+}
 
-  async function scrapeProductData(products) {
+async function scrapeProductData(products) {
 
     const results = await Promise.all(products.map(async (product) => {
       const productDetails = {};
@@ -674,246 +454,8 @@ async function exportToCSV(productData) {
         console.error(`File ${filePath} was not created.`);
     }
 
-    // ส่งไฟล์ให้ผู้ใช้ดาวน์โหลด
-    res.download(filePath, (err) => {
-        if (err) {
-            console.log('Error downloading file:', err);
-        } else {
-            console.log('File downloaded:', filePath);
-        }
-    });
   }
+    
+
   await browser.close();
-});
-
-
-
-app.post('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            return res.status(500).send('Error logging out');
-        }
-        res.redirect('/Adminlogin'); // เมื่อออกจากระบบแล้วให้กลับไปที่หน้า login
-    });
-});
-
-
-// Route สำหรับแสดงรายการในรูปแบบ pagination
-
-
-// หน้าแรก
-app.get('/', (req, res) => {
-    const specs = [];
-    const specsJIB = []; // กำหนดค่าเริ่มต้นเป็นว่างเปล่า
-    res.render('index', { specs: specs, specsJIB: specsJIB, query: '' }); // เริ่มที่หน้าแรก
-});
-
-app.get('/search', async (req, res) => {
-    const query = req.query.query || ''; // รับคำค้นจากฟอร์ม
-
-    try {
-        // ค้นหาใน MongoDB โดยเช็คจาก ModelCPU หรือ ModelMainboard หรือ ModelVGA
-        const specs = await Spec.find({
-            $or: [
-                { ModelCPU: { $regex: query, $options: 'i' } },
-                { BrandCPU: { $regex: query, $options: 'i' } },
-                { ModelVGA: { $regex: query, $options: 'i' } },
-                { Rank1: { $regex: query, $options: 'i' }},
-            ]
-        });
-
-        const specsJIB = await SpecJIB.find({
-            $or: [
-                { ModelCPU: { $regex: query, $options: 'i' } },
-                { BrandCPU: { $regex: query, $options: 'i' } },
-                { ModelVGA: { $regex: query, $options: 'i' } },
-                { Rank1: { $regex: query, $options: 'i' }},
-                { ModelCom: { $regex: query, $options: 'i' }},
-            ]
-        });
-
-        // ส่งข้อมูลผลการค้นหากลับไปที่หน้า index
-        res.render('index', { specs: specs, specsJIB: specsJIB, query: query });
-    } catch (err) {
-        res.status(500).send('Error retrieving specs');
-    }
-});
-
-// หน้า com_list
-app.get('/com_list', (req, res) => {
-    res.redirect('/com_list/page/1');
-});
-
-app.get('/com_list/page/:pageNumber', async (req, res) => {
-    const page = parseInt(req.params.pageNumber) || 1; // หน้าที่ผู้ใช้ขอ
-    const limit = 10; // จำนวนรายการต่อหน้า
-    const skip = (page - 1) * limit;
-
-    try {
-        // ดึงข้อมูลจาก MongoDB collection 'speccom' พร้อม pagination
-        const specs = await Spec.find().skip(skip).limit(limit);
-
-        // ดึงข้อมูลทั้งหมดเพื่อคำนวณจำนวนหน้า
-        const totalSpecs = await Spec.countDocuments();
-
-        const totalPages = Math.ceil(totalSpecs / limit); // คำนวณจำนวนหน้าทั้งหมด
-
-        specs.sort((a, b) => a.ModelCPU.localeCompare(b.ModelCPU));
-
-        // ส่งข้อมูลไปยังหน้า com_list
-        res.render('com_list', { 
-            specs: specs, 
-            currentPage: page, 
-            totalPages: totalPages, 
-            query: ''
-        });
-    } catch (err) {
-        res.status(500).send('Error retrieving specs');
-    }
-});
-
-app.get('/com_list/search', async (req, res) => {
-    const query = req.query.query || ''; // รับคำค้น
-    const page = parseInt(req.query.page) || 1; // หน้าที่ผู้ใช้ขอ
-    const limit = 10; // จำนวนรายการต่อหน้า
-    const skip = (page - 1) * limit;
-
-
-    try {
-        // ค้นหาจากชื่อคอมพิวเตอร์หรือซีพียู
-        const specs = await Spec.find({
-            $or: [
-                { ModelCPU: { $regex: query, $options: 'i' } },
-                { BrandCPU: { $regex: query, $options: 'i' } },
-                { ModelVGA: { $regex: query, $options: 'i' } },
-                { Rank1: { $regex: query, $options: 'i' }},
-            ]
-        })
-        .skip(skip)
-        .limit(limit);
-
-        const totalSpecs = await Spec.countDocuments({
-            $or: [
-                { ModelCPU: { $regex: query, $options: 'i' } },
-                { BrandCPU: { $regex: query, $options: 'i' } },
-                { ModelVGA: { $regex: query, $options: 'i' } },
-                { Rank1: { $regex: query, $options: 'i' }},
-            ]
-        });
-
-        const totalPages = Math.ceil(totalSpecs / limit); // คำนวณจำนวนหน้าทั้งหมด
-        const maxPagesToShow = 5; // จำนวนหน้าที่จะแสดง
-        let startPage = Math.max(1, page - Math.floor(maxPagesToShow / 2));
-        let endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
-
-        specs.sort((a, b) => a.ModelCPU.localeCompare(b.ModelCPU));
-
-        // ถ้าหน้าสุดท้ายเกิน totalPages ให้ขยับ startPage
-        if (endPage - startPage < maxPagesToShow - 1) {
-            startPage = Math.max(1, endPage - maxPagesToShow + 1);
-        }
-
-        res.render('com_list', {
-            specs: specs,
-            currentPage: page,
-            totalPages: totalPages,
-            query: query,
-            startPage: startPage,
-            endPage: endPage,
-        });
-    } catch (err) {
-        res.status(500).send('Error searching specs');
-    }
-});
-
-app.get('/com_list_brand', (req, res) => {
-    res.redirect('/com_list_brand/page/1');
-});
-
-app.get('/com_list_brand/page/:pageNumber', async (req, res) => {
-    const page = parseInt(req.params.pageNumber) || 1; // หน้าที่ผู้ใช้ขอ
-    const limit = 10; // จำนวนรายการต่อหน้า
-    const skip = (page - 1) * limit;
-
-    try {
-        // ดึงข้อมูลจาก MongoDB collection 'speccom' พร้อม pagination
-        const specs = await SpecJIB.find().skip(skip).limit(limit);
-
-        // ดึงข้อมูลทั้งหมดเพื่อคำนวณจำนวนหน้า
-        const totalSpecs = await SpecJIB.countDocuments();
-
-        const totalPages = Math.ceil(totalSpecs / limit); // คำนวณจำนวนหน้าทั้งหมด
-
-        // ส่งข้อมูลไปยังหน้า com_list
-        res.render('com_list_brand', { 
-            specs: specs, 
-            currentPage: page, 
-            totalPages: totalPages, 
-            query: ''
-        });
-    } catch (err) {
-        res.status(500).send('Error retrieving specs');
-    }
-});
-
-app.get('/com_list_brand/search', async (req, res) => {
-    const query = req.query.query || ''; // รับคำค้น
-    const page = parseInt(req.query.page) || 1; // หน้าที่ผู้ใช้ขอ
-    const limit = 10; // จำนวนรายการต่อหน้า
-    const skip = (page - 1) * limit;
-
-
-    try {
-        // ค้นหาจากชื่อคอมพิวเตอร์หรือซีพียู
-        const specs = await SpecJIB.find({
-            $or: [
-                { ModelCPU: { $regex: query, $options: 'i' } },
-                { BrandCPU: { $regex: query, $options: 'i' } },
-                { ModelVGA: { $regex: query, $options: 'i' } },
-                { Rank1: { $regex: query, $options: 'i' }},
-                { ModelCom: { $regex: query, $options: 'i' }},
-            ]
-        })
-        .skip(skip)
-        .limit(limit);
-
-        const totalSpecs = await SpecJIB.countDocuments({
-            $or: [
-                { ModelCPU: { $regex: query, $options: 'i' } },
-                { BrandCPU: { $regex: query, $options: 'i' } },
-                { ModelVGA: { $regex: query, $options: 'i' } },
-                { Rank1: { $regex: query, $options: 'i' }},
-                { ModelCom: { $regex: query, $options: 'i' }},
-            ]
-        });
-
-        const totalPages = Math.ceil(totalSpecs / limit); // คำนวณจำนวนหน้าทั้งหมด
-        const maxPagesToShow = 5; // จำนวนหน้าที่จะแสดง
-        let startPage = Math.max(1, page - Math.floor(maxPagesToShow / 2));
-        let endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
-
-        // ถ้าหน้าสุดท้ายเกิน totalPages ให้ขยับ startPage
-        if (endPage - startPage < maxPagesToShow - 1) {
-            startPage = Math.max(1, endPage - maxPagesToShow + 1);
-        }
-
-        res.render('com_list_brand', {
-            specs: specs,
-            currentPage: page,
-            totalPages: totalPages,
-            query: query,
-            startPage: startPage,
-            endPage: endPage,
-        });
-    } catch (err) {
-        res.status(500).send('Error searching specs');
-    }
-});
-
-app.get('/companent', (req, res) => {
-    res.render('components')
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+})();
